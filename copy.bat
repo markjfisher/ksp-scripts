a8cd6ccf1b11712f5572e07823e414f9eb71c229
Mark Fisher <mark.j.fisher@gmail.com>
2022-10-03 09:03:29 +0100

    work on missions names and running them #2

--- copy.bat
+++ copy.bat
@@ -6,8 +6,8 @@ echo "Deleting old scripts"
 del /s /q %KSP_FOLDER%\*
 
 echo "minifying src"
-del /s /q scripts/lib/*
-del /s /q scripts/missions/*
+del /s /q scripts\lib\*
+del /s /q scripts\missions\*
 
 c:\msys64\usr\bin\env MSYSTEM=MINGW64 c:\msys64\usr\bin\bash -l -c "cd /d/dev/kos/ksp1; ./minify.sh"
 echo "done"
